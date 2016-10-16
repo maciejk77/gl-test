c9457d7360fb0a78013269cf17596f0892cef65f
Maciej Kurek <maciejk77@gmail.com>
2016-10-16 13:45:11 +0100

    First commit, BEM component in index.html and classes is Sassfile

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const cssmin = require('gulp-cssmin');
+const uglify = require('gulp-uglify');
+const del     = require('del');
+
+const files = {
+  styles: 'src/styles/*.scss',
+  js: [
+    'bower_components/jquery/dist/jquery.js',
+    'src/js/*.js'
+  ]
+}
+
+gulp.task('default', [
+  'clean',
+  'styles',
+  'js',
+  'watch'
+]);
+
+gulp.task('watch', function(){
+  
+  gulp.watch(files.styles, ['styles']);
+  gulp.watch(files.js, ['js']);
+  
+});
+
+gulp.task('styles', function() {
+  
+  gulp.src(files.styles)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cssmin())
+    .pipe(concat('styles.min.css'))
+    .pipe(gulp.dest('dist/styles'));
+  
+});
+
+gulp.task('js', function() {
+  
+  gulp.src(files.js)
+    .pipe(concat('javascript.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'));
+  
+});
+
+gulp.task('clean', function() { 
+  del.sync(['dist']);
+});
---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  
+  console.log("Hello :)");
+  
+  // modular jQuery, design pattern? init, render etc? (Maciek)
+
+});
