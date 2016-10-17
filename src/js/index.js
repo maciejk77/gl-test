8df0e8e39c4630493139e6f90e82a3e8e622643f
Maciej Kurek <maciejk77@gmail.com>
2016-10-17 22:06:30 +0100

    Experiment with Procfile for Heroku deployment

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
-$(document).ready(function() {
-  
-  console.log("Hello :)");
-  
-  // modular jQuery, design pattern? init, render etc? (Maciek)
+(function() {
 
-});
+  $(".info-banner__title").click(function() {
+    $(this).toggle();
+    });
+})();
